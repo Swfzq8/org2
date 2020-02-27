e306b9fc4553709acc5a5023c12dcee3884bf3cc
Nick-03 <58921151+Nick-03@users.noreply.github.com>
2020-02-27 10:53:22 -0600

    Update inst.s

--- src/inst.s
+++ src/inst.s
@@ -21,6 +21,7 @@ addiu $r0, $zero, 0x2
 sw $r0, 0x20($r4)
 addiu $r0, $zero, 0xA
 sw $r0, 0x24($r4)
+lw $r1, $r4
 addiu $r2, $zero, 0x9
 addiu $r3, $zero, 0x0
 addiu $r6, $zero, 0x9
@@ -35,7 +36,7 @@ sub $r7, $r7, $r8
 bgtz $r7, 0x0009 
 addiu $r3, $r3, 0x1 
 bne $r2, $r3, 0xFFF8 
-lw $r4, 0xFFDC($r4)
+lw $r4, $r1
 addiu $r5, $r5, 0x1 
 bne $r6, $r5, 0xFFF4 
 bne $r10, $r11, 0x0005 
