da955de1a7daee33bcea1a6c1d80d8aca08de93c
Swfzq8 <54684977+Swfzq8@users.noreply.github.com>
2020-02-27 13:52:15 -0600

    reworking bubblesort

--- src/inst.s
+++ src/inst.s
@@ -20,24 +20,15 @@ sw $r0, 0x20($r4)
 addiu $r0, $zero, 0xA
 sw $r0, 0x24($r4)
 lw $r1, $r4
-addiu $r2, $zero, 0x9
-addiu $r3, $zero, 0x0
-addiu $r6, $zero, 0x9
-addiu $r5, $zero, 0x0
-addiu $r3, $zero, 0x0
+addiu $r15, $zero, 0x9
+addiu $r17, $zero, 0x0
 lw $r7, 0x0($r4) 
 lw $r8, 0x4($r4) 
-lw $r4, 0x4($r4) 
-addiu $r10, $zero, 0x0
-lw $r9, $r7
-sub $r7, $r7, $r8
-bgtz $r7, 0x0007
-addiu $r3, $r3, 0x1 
-bne $r2, $r3, 0xFFF8 
-lw $r4, $r1
-addiu $r5, $r5, 0x1 
-bne $r6, $r5, 0xFFF4 
-bne $r10, $r6, 0x0005 
-sw $r9, 0x0($r4)
-sw $r8, 0x4($r4)
-bne $r10, $r11, 0xFFF8 
+lw $r1, 0x4($r4)
+addiu $r17, $r17, 0x1
+sub $r3, $r7, $r1
+bgtz $r3 0xfffb
+sw $r1, 0x0($r4)
+sw $r7 ,0x4($r4)
+bne $r15, $r17, 0xfff8 
+ 
